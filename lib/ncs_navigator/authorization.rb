818fd7a153cffd40f807dcb7c43f43bee22522b1
Jalpa Patel <jalpa-patel@northwestern.edu>
2012-02-24 13:38:45 -0600

    Corrected the module structure.

---
+++ lib/ncs_navigator/authorization.rb
@@ -0,0 +1,7 @@
+module NcsNavigator
+  module Authorization
+    autoload :Core, 'ncs_navigator/authorization/core'
+    autoload :StaffPortal, 'ncs_navigator/authorization/staff_portal'
+    autoload :Psc, 'ncs_navigator/authorization/psc'
+  end
+end
--- lib/ncs_navigator/authorization/authorization.rb
+++
@@ -1,5 +0,0 @@
-module NcsNavigator::Authorization
-  autoload :Core, 'ncs_navigator/authorization/core'
-  autoload :StaffPortal, 'ncs_navigator/authorization/staff_portal'
-  autoload :Psc, 'ncs_navigator/authorization/psc'
-end
